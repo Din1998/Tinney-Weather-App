318e6ac59581a793e74fe201868ef99ecf267f3d
din1998 <dinislam8803@gmail.com>
2023-01-22 16:15:50 +0600

    Change some ui

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+
+
+
+export default function Footer() {
+  return(
+    <div className='footer__section'>
+      <div className='footer__copyright'>
+        <p>© BD Weather APP 2023</p>
+      </div>
+      <a className="link" href="https://dinislam.vercel.app" target="blank">
+        <div className='creator__site'>
+          <h1>Din</h1>
+        </div>
+      </a>
+      
+    </div>
+  )
+}
